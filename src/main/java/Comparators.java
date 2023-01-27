6f6346485ccad30f1364f8d9d487ebd5da18b95c
Chichior <katiehakala@icloud.com>
2023-01-27 02:13:52 +0000

    ComparatorsComplete

--- src/main/java/Comparators.java
+++ src/main/java/Comparators.java
@@ -18,13 +18,19 @@
  * boolean comparison operators here: https://www.oreilly.com/library/view/java-for-dummies/9781118239742/a17.html
  */
 public class Comparators {
+    public boolean isEqual(int a, int b){
+    
+        
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is equal to b, false otherwise.
      */
-    public boolean isEqual(int a, int b){
-        return false;
+        if(a == b){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -33,7 +39,11 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
-        return false;
+        if(a > b){
+            return true;
+        } else {
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -41,7 +51,11 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return false;
+        if(a < b){
+            return true;
+        } else {
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -49,7 +63,11 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        if(a >= b){
+            return true;
+        } else {
+            return false;
+        }
     }
     /**
      * @param a the first number to be compared.
@@ -57,7 +75,11 @@ public class Comparators {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+        if(a <= b){
+            return true;
+        } else {
+            return false;
+        }    
     }
 
     /**
@@ -66,6 +88,10 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        return false;
+        if(a != b){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
